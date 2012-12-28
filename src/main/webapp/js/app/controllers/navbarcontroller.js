36c85e31ab53d61116bd611ef0d4f56ec97aa3ae
dario <dar.fabbri@gmail.com>
2012-12-29 00:29:04 +0100

    Start of binding automatic menu items enable and disable to user grants.

--- src/main/webapp/js/app/controllers/navbarcontroller.js
+++ src/main/webapp/js/app/controllers/navbarcontroller.js
@@ -1,5 +1,21 @@
 (function(app) {
 	
-	app.NavbarController = Ember.Controller.extend();
+	app.NavbarController = Ember.Controller.extend({
+		
+		grants: [],
+		
+		loadGrants: function() {
+			
+			grants.push({
+		        "action": "test",
+		        "allowed": true
+		    });
+		},
+		
+		checkGrant: function(action) {
+			
+			return false;
+		}
+	});
 	
 })(window.App);
--- src/main/webapp/js/app/views/navbarview.js
+++ src/main/webapp/js/app/views/navbarview.js
@@ -2,7 +2,43 @@
 	
 	app.NavbarView = Ember.View.extend({
 		
-		template: TemplatesUtil.compile('navbar.html')
+		template: TemplatesUtil.compile('navbar.html'),
+		
+		didInsertElement: function() {
+			
+			// Right after element rendering, disable all actions.
+			//
+			this.processActions();
+		},
+	
+		processActions: function() {
+			
+			// Find all elements having data-action attribute.
+			//
+			var that = this;
+			$('[data-action]').each(function() {
+				
+				// Get action.
+				//
+				var action = $(this).attr('data-action');
+				if(!that.controller.checkGrant(action)) {
+					$(this).addClass('disabled');
+				}
+			});
+		},
+		
+		grantsDidChange: function() {
+			
+			this.processActions();
+			
+		}.observes('controller.grants'),
+		
+		click: function(event) {
+			
+			if($(event.target).parent().hasClass('disabled')) {
+				event.preventDefault();
+			}
+		}
 	});
 	
 })(window.App);
